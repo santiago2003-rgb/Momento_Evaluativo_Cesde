175b5a868c0be74e64aaa7e5cf5c0d5a8ff6bc65
santiago2003-rgb <santiagomejiagar@gmail.com>
2025-08-21 19:57:34 -0500

    se ponen comentarios

--- ingesta_y_validacion.py
+++ ingesta_y_validacion.py
@@ -5,21 +5,22 @@ def cargar_compras(ruta):
         lineas = archivo.readlines()
         
 
-    for i, linea in enumerate(lineas[1:], start=2):  # Ignorar encabezado
+    for i, linea in enumerate(lineas[1:], start=2): # Saltar encabezado
         if not linea.strip():
-            continue  # saltar líneas vacías
+            continue
 
         partes = linea.strip().split(",")
 
         cliente, fecha, producto, cantidad_str, precio_str = partes
-
+        
+        # Maneja las exepciones verificando si no son numeros
         try:
             cantidad = int(cantidad_str)
             precio_unitario = float(precio_str)
         except:
             print(f"⚠️ Línea {i}: cantidad o precio no son números.")
             continue
-
+        
         if cantidad <= 0 or precio_unitario <= 0:
             print(f"⚠️ Línea {i}: cantidad o precio <= 0.")
             continue
